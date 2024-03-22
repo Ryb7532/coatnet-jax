864c8f0f1e7a7ed2cc50ab579e6bec515a6bdcd6
Ryb7532 <ryb09137532@gmail.com>
2024-03-22 12:42:27 +0900

    add args of 'dtype' and 'param_dtype'

--- coatnet.py
+++ coatnet.py
@@ -4,22 +4,30 @@ import jax
 import jax.numpy as jnp
 
 from flax import linen as nn
-from clu import parameter_overview
 
 Array = Union[jax.Array, Any]
+Dtype = Union[jax.typing.DTypeLike, Any]
 
 
 class ConvStem(nn.Module):
     features: int
     stride: int
     activation: Callable[..., Array] = nn.gelu
+    dtype: Optional[Dtype] = None
+    param_dtype: Dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x: Array, train: bool = False) -> Array:
         strides = (self.stride,) * 2
-        x = nn.Conv(features=self.features, kernel_size=(3,3), strides=strides, padding='SAME',
-                    use_bias=False)(x)
-        x = nn.BatchNorm(use_running_average=not train)(x)
+        x = nn.Conv(
+                features=self.features,
+                kernel_size=(3,3),
+                strides=strides,
+                padding='SAME',
+                use_bias=False,
+                dtype=self.dtype,
+                param_dtype=self.param_dtype)(x)
+        x = nn.BatchNorm(use_running_average=not train, dtype=self.dtype, param_dtype=self.param_dtype)(x)
         x = self.activation(x)
         return x
 
@@ -31,6 +39,8 @@ class MBConv(nn.Module):
     se_ratio: float = 0.25
     activation: Callable[..., Array] = nn.gelu
     dropout_rate: float = 0.0
+    dtype: Optional[Dtype] = None
+    param_dtype: Dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x: Array, train: bool = False) -> Array:
@@ -38,33 +48,64 @@ class MBConv(nn.Module):
         strides = (self.stride,) * 2
         if self.stride > 1:
             res = nn.max_pool(res, window_shape=(3,3), strides=strides, padding='SAME')
-            res = nn.Conv(features=self.features, kernel_size=(1,1), strides=(1,1))(res)
+            res = nn.Conv(
+                    features=self.features,
+                    kernel_size=(1,1),
+                    strides=(1,1),
+                    dtype=self.dtype,
+                    param_dtype=self.param_dtype)(res)
 
-        x = nn.BatchNorm(use_running_average=not train)(x)
+        x = nn.BatchNorm(use_running_average=not train, dtype=self.dtype, param_dtype=self.param_dtype)(x)
 
         mid_channels = int(x.shape[-1] * self.expand_ratio)
 
-        x = nn.Conv(features=mid_channels, kernel_size=(1,1), strides=strides, use_bias=False)(x)
+        x = nn.Conv(
+                features=mid_channels,
+                kernel_size=(1,1),
+                strides=strides,
+                use_bias=False,
+                dtype=self.dtype,
+                param_dtype=self.param_dtype)(x)
         # From eqn (5), downsampling is applied here
-        x = nn.BatchNorm(use_running_average=not train)(x)
+        x = nn.BatchNorm(use_running_average=not train, dtype=self.dtype, param_dtype=self.param_dtype)(x)
         x = self.activation(x)
 
         # Depthwise Convolution
-        x = nn.Conv(features=mid_channels, kernel_size=(3,3), strides=(1,1), padding='SAME', use_bias=False,
-                    feature_group_count=mid_channels)(x)
-        x = nn.BatchNorm(use_running_average=not train)(x)
+        x = nn.Conv(
+                features=mid_channels,
+                kernel_size=(3,3),
+                strides=(1,1),
+                padding='SAME',
+                use_bias=False,
+                feature_group_count=mid_channels,
+                dtype=self.dtype,
+                param_dtype=self.param_dtype)(x)
+        x = nn.BatchNorm(use_running_average=not train, dtype=self.dtype, param_dtype=self.param_dtype)(x)
         x = self.activation(x)
 
         # Squeeze and Excitation
         scale = jnp.mean(x, axis=(1,2), keepdims=True) # assuming (batch, height, width, channels)
         sqz_channels = max(1, int(scale.shape[-1] * self.se_ratio))
-        scale = nn.Conv(features=sqz_channels, kernel_size=(1,1))(scale)
+        scale = nn.Conv(
+                    features=sqz_channels,
+                    kernel_size=(1,1),
+                    dtype=self.dtype,
+                    param_dtype=self.param_dtype)(scale)
         scale = self.activation(scale)
-        scale = nn.Conv(features=mid_channels, kernel_size=(1,1))(scale)
+        scale = nn.Conv(
+                    features=mid_channels,
+                    kernel_size=(1,1),
+                    dtype=self.dtype,
+                    param_dtype=self.param_dtype)(scale)
         scale = nn.sigmoid(x)
         x = x * scale
 
-        x = nn.Conv(features=self.features, kernel_size=(1,1), strides=(1,1))(x)
+        x = nn.Conv(
+                features=self.features,
+                kernel_size=(1,1),
+                strides=(1,1),
+                dtype=self.dtype,
+                param_dtype=self.param_dtype)(x)
         if self.dropout_rate:
             x = nn.Dropout(rate=self.dropout_rate)(x, deterministic=not train)
 
@@ -82,9 +123,9 @@ def build_relative_position_index(height, width):
     return relative_coords.sum(-1).reshape((-1))
 
 
-def build_relative_position_table(rng, height, width, n_heads):
+def build_relative_position_table(rng, height, width, n_heads, param_dtype=jnp.float32):
     initializer = nn.initializers.normal(0.02)
-    return initializer(rng, ((2 * height - 1) * (2 * width - 1), n_heads), jnp.float32)
+    return initializer(rng, ((2 * height - 1) * (2 * width - 1), n_heads), param_dtype)
 
 
 class RelativeMultiHeadSelfAttention(nn.Module):
@@ -92,6 +133,8 @@ class RelativeMultiHeadSelfAttention(nn.Module):
     head_dim: int = 32
     n_heads: Optional[int] = None
     dropout_rate: float = 0.0
+    dtype: Optional[Dtype] = None
+    param_dtype: Dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x: Array, train: bool = False) -> Array:
@@ -104,13 +147,17 @@ class RelativeMultiHeadSelfAttention(nn.Module):
         relative_position_index = self.variable('immutable', 'relative_position_index',
                                                 build_relative_position_index, H, W)
         relative_position_table = self.param('relative_position_bias_table', build_relative_position_table,
-                                             H, W, n_heads)
+                                             H, W, n_heads, self.param_dtype)
 
         x = x.reshape((B, H*W, C))
 
-        q = nn.Dense(hidden_dim)(x).reshape((B, -1, n_heads, self.head_dim)).transpose((0, 2, 1, 3))
-        k = nn.Dense(hidden_dim)(x).reshape((B, -1, n_heads, self.head_dim)).transpose((0, 2, 3, 1))
-        v = nn.Dense(hidden_dim)(x).reshape((B, -1, n_heads, self.head_dim)).transpose((0, 2, 1, 3))
+        q = nn.Dense(hidden_dim, dtype=self.dtype, param_dtype=self.param_dtype)(x)
+        k = nn.Dense(hidden_dim, dtype=self.dtype, param_dtype=self.param_dtype)(x)
+        v = nn.Dense(hidden_dim, dtype=self.dtype, param_dtype=self.param_dtype)(x)
+
+        q = q.reshape((B, -1, n_heads, self.head_dim)).transpose((0, 2, 1, 3))
+        k = k.reshape((B, -1, n_heads, self.head_dim)).transpose((0, 2, 3, 1))
+        v = v.reshape((B, -1, n_heads, self.head_dim)).transpose((0, 2, 1, 3))
 
         score = jnp.matmul(q, k) * (hidden_dim ** -0.5)
         pos_bias = jnp.take(relative_position_table, relative_position_index.value, 0)
@@ -124,7 +171,7 @@ class RelativeMultiHeadSelfAttention(nn.Module):
 
         out = attn.transpose(0, 2, 1, 3)
         out = out.reshape((B, -1, hidden_dim))
-        out = nn.Dense(features=self.features)(out)
+        out = nn.Dense(features=self.features, dtype=self.dtype, param_dtype=self.param_dtype)(out)
 
         return out.reshape((B, H, W, -1))
 
@@ -137,33 +184,40 @@ class ConvTransformer(nn.Module):
     activation: Callable[..., Array] = nn.gelu
     dropout_rate: float = 0.0
     downsampling: bool = False
+    dtype: Optional[Dtype] = None
+    param_dtype: Dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x: Array, train: bool = False) -> Array:
         proj = x
 
-        x = nn.LayerNorm()(x)
+        x = nn.LayerNorm(dtype=self.dtype, param_dtype=self.param_dtype)(x)
 
         if self.downsampling or x.shape[-1] != self.features:
             if self.downsampling:
                 proj = nn.max_pool(x, window_shape=(3,3), strides=(2,2), padding='SAME')
                 x = nn.max_pool(x, window_shape=(3,3), strides=(2,2), padding='SAME')
-            proj = nn.Conv(self.features, kernel_size=(1,1))(proj)
+            proj = nn.Conv(self.features, kernel_size=(1,1), dtype=self.dtype, param_dtype=self.param_dtype)(proj)
 
-        x = RelativeMultiHeadSelfAttention(features=self.features, head_dim=self.head_dim, n_heads=self.n_heads,
-                                           dropout_rate=self.dropout_rate)(x)
+        x = RelativeMultiHeadSelfAttention(
+                features=self.features,
+                head_dim=self.head_dim,
+                n_heads=self.n_heads,
+                dropout_rate=self.dropout_rate,
+                dtype=self.dtype,
+                param_dtype=self.param_dtype)(x)
 
         x = x + proj
 
         out = x
         hidden_dim = int(self.features * self.expand_ratio)
 
-        x = nn.LayerNorm()(x)
-        x = nn.Dense(hidden_dim)(x)
+        x = nn.LayerNorm(dtype=self.dtype, param_dtype=self.param_dtype)(x)
+        x = nn.Dense(hidden_dim, dtype=self.dtype, param_dtype=self.param_dtype)(x)
         x = self.activation(x)
         if self.dropout_rate:
             x = nn.Dropout(self.dropout_rate)(x, deterministic=not train)
-        x = nn.Dense(out.shape[-1])(x)
+        x = nn.Dense(out.shape[-1], dtype=self.dtype, param_dtype=self.param_dtype)(x)
         if self.dropout_rate:
             x = nn.Dropout(self.dropout_rate)(x, deterministic=not train)
 
@@ -179,6 +233,8 @@ class CoAtNet(nn.Module):
     dropout_rate: float = 0.0
     head_dim: int = 32
     n_heads: Optional[int] = None
+    dtype: Optional[Dtype] = None
+    param_dtype: Dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x: Array, train: bool = False) -> Array:
@@ -187,24 +243,41 @@ class CoAtNet(nn.Module):
                 downsampling = downsampling and j == 0
                 stride = 2 if downsampling else 1
                 if b_type.upper() == 'C':
-                    x = ConvStem(features=out_channels, stride=stride, activation=self.activation,
-                                 name='stage{0}_block{1}'.format(i, j))(x, train)
+                    x = ConvStem(
+                            features=out_channels,
+                            stride=stride,
+                            activation=self.activation,
+                            dtype=self.dtype,
+                            param_dtype=self.param_dtype,
+                            name='stage{0}_block{1}'.format(i, j))(x, train)
                 elif b_type.upper() == 'M':
                     x = MBConv(
-                        features=out_channels, stride=stride, expand_ratio=self.expand_ratio,
-                        se_ratio=self.se_ratio, activation=self.activation, dropout_rate=self.dropout_rate,
-                        name='stage{0}_block{1}'.format(i, j))(x, train)
+                            features=out_channels,
+                            stride=stride,
+                            expand_ratio=self.expand_ratio,
+                            se_ratio=self.se_ratio,
+                            activation=self.activation,
+                            dropout_rate=self.dropout_rate,
+                            dtype=self.dtype,
+                            param_dtype=self.param_dtype,
+                            name='stage{0}_block{1}'.format(i, j))(x, train)
                 elif b_type.upper() == 'T':
-                    pass
-                    x = ConvTransformer(features=out_channels, head_dim=self.head_dim, n_heads=self.n_heads,
-                                        expand_ratio=self.expand_ratio, activation=self.activation,
-                                        dropout_rate=self.dropout_rate, downsampling=downsampling,
-                                        name='stage{0}_block{1}'.format(i, j))(x, train)
+                    x = ConvTransformer(
+                            features=out_channels,
+                            head_dim=self.head_dim,
+                            n_heads=self.n_heads,
+                            expand_ratio=self.expand_ratio,
+                            activation=self.activation,
+                            dropout_rate=self.dropout_rate,
+                            downsampling=downsampling,
+                            dtype=self.dtype,
+                            param_dtype=self.param_dtype,
+                            name='stage{0}_block{1}'.format(i, j))(x, train)
 
         x = jnp.mean(x, axis=(1,2)) # assuming (batch, height, width, channels)
         if self.dropout_rate > 0:
             x = nn.Dropout(self.dropout_rate)(x, deterministic=not train)
-        x = nn.Dense(self.num_classes)(x)
+        x = nn.Dense(self.num_classes, dtype=self.dtype, param_dtype=self.param_dtype)(x)
 
         return x
 
